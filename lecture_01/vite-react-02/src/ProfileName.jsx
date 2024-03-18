117315a0bb3adde643f5862c7ebefd51e8dfad1c
bojanangelis <bojanangeleski98@gmail.com>
2024-03-18 22:02:05 +0100

    lecture 01

---
+++ lecture_01/vite-react-02/src/ProfileName.jsx
@@ -0,0 +1,11 @@
+function ProfileName() {
+    return (
+        <div className="profile-name">
+            <h2>Profile Name</h2>
+            <p>Profile description</p>
+            <p>Profile description</p>
+        </div>
+    )
+}
+
+export default ProfileName;
---
+++ lecture_01/vite-react-02/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = () => {
+  return (
+    <div>
+      {/* props */}
+      <Button title='text' /> 
+      <Button title='foloower' />
+      <Button title='my network' />
+
+    </div>
+  )
+}
+
+export default Header
